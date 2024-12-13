747d78ffcf574fb3d3fd65de6657d46fe2be3f4b
Lauren Tomasi <ltomasi@MacBook-Pro-4.lan>
2024-12-12 17:26:24 -0800

    Working demo

--- app.js
+++ app.js
@@ -1,30 +1,10 @@
 // Placeholder for future interactivity
 console.log("FoodieDash App Loaded");
 
-// Example functionality: Alert when clicking a restaurant
-document.querySelectorAll('.restaurant-card').forEach(card => {
-    card.addEventListener('click', () => {
-        alert(`${card.querySelector('h3').textContent} selected!`);
-    });
-});
-
-// Example functionality: Alert when clicking a menu item
+// Updates menu item style on click
 document.querySelectorAll('.menu-item').forEach(card => {
     card.addEventListener('click', () => {
-        alert(`${card.querySelector('strong').textContent} selected!`);
-    });
-});
-
-// Example functionality: Alert when adding to cart
-document.querySelectorAll('.add-to-cart-section').forEach(card => {
-    card.addEventListener('click', () => {
-        alert(`${card.querySelector('h3').textContent} selected!`);
-    });
-});
-
-// Example functionality: Alert when confirming order
-document.querySelectorAll('.confirm-order').forEach(card => {
-    card.addEventListener('click', () => {
-        alert(`${card.querySelector('h3').textContent} selected!`);
+        //alert(`${card.querySelector('strong').textContent} selected!`);
+        card.classList.contains('menu-item-click') ? card.classList.remove('menu-item-click') : card.classList.add('menu-item-click');
     });
 });
